570e5346e6fef316d6477e1c7d9349663f629ae1
SputnikRocket <sputnikchicken08@gmail.com>
2023-09-23 12:41:01 -0400

    moved devicetree paths to board-specific configs

--- configs/boards.sh
+++ configs/boards.sh
@@ -8,77 +8,77 @@ trap 'echo Error: in $0 on line $LINENO' ERR
 #Orange Pi 5 Plus
 function config-orangepi-5plus {
 	
-	DEVICETREE="$DTB_OPI5PLUS"
+	DEVICETREE="dtbs/rockchip/rk3588-orangepi-5-plus.dtb"
 	IMAGEFILE="${IMGPREFIX}-OrangePi_5Plus-${PROFILE}-UEFI.img"
 }
 
 #Orange Pi 5
 function config-orangepi-5 {
 	
-	DEVICETREE="$DTB_OPI5"
+	DEVICETREE="dtbs/rockchip/rk3588s-orangepi-5.dtb"
 	IMAGEFILE="${IMGPREFIX}-OrangePi_5-${PROFILE}-UEFI.img"
 }
 
 #Orange Pi 5B
 function config-orangepi-5b {
 	
-	DEVICETREE="$DTB_OPI5B"
+	DEVICETREE="dtbs/rockchip/rk3588s-orangepi-5b.dtb"
 	IMAGEFILE="${IMGPREFIX}-OrangePi_5B-${PROFILE}-UEFI.img"
 }
 
 #Rock 5A
 function config-rock-5a {
 	
-	DEVICETREE="$DTB_ROCK5A"
+	DEVICETREE="dtbs/rockchip/rk3588s-rock-5a.dtb"
 	IMAGEFILE="${IMGPREFIX}-Rock_5A-${PROFILE}-UEFI.img"
 }
 
 #Rock 5B
 function config-rock-5b {
 	
-	DEVICETREE="$DTB_ROCK5B"
+	DEVICETREE="dtbs/rockchip/rk3588-rock-5b.dtb"
 	IMAGEFILE="${IMGPREFIX}-Rock_5B-${PROFILE}-UEFI.img"
 }
 
 #Indiedroid Nova
 function config-indiedroid-nova {
 	
-	DEVICETREE="$DTB_IDNOVA"
+	DEVICETREE="dtbs/rockchip/rk3588s-9tripod-linux.dtb"
 	IMAGEFILE="${IMGPREFIX}-Indiedroid_Nova-${PROFILE}-UEFI.img"
 }
 
 #NanoPi R6C
 function config-nanopi-r6c {
 	
-	DEVICETREE="$DTB_NANOPI_R6C"
+	DEVICETREE="dtbs/rockchip/rk3588s-nanopi-r6c.dtb"
 	IMAGEFILE="${IMGPREFIX}-NanoPi_R6C-${PROFILE}-UEFI.img"
 }
 
 #NanoPi R6S
 function config-nanopi-r6s {
 	
-	DEVICETREE="$DTB_NANOPI_R6S"
+	DEVICETREE="dtbs/rockchip/rk3588s-nanopi-r6s.dtb"
 	IMAGEFILE="${IMGPREFIX}-NanoPi_R6S-${PROFILE}-UEFI.img"
 }
 
 #NanoPC T6
 function config-nanopc-t6 {
 	
-	DEVICETREE="$DTB_NANOPC_T6"
+	DEVICETREE="dtbs/rockchip/rk3588-nanopc-t6.dtb"
 	IMAGEFILE="${IMGPREFIX}-NanoPC_T6-${PROFILE}-UEFI.img"
 }
 
 #Khadas Edge2
 function config-khadas-edge2 {
 	
-	DEVICETREE="$DTB_KHADAS_E2"
+	DEVICETREE="dtbs/rockchip/rk3588s-khadas-edge2.dtb"
 	IMAGEFILE="${IMGPREFIX}-Khadas_Edge2-${PROFILE}-UEFI.img"
 }
 
 #Mixtile Blade3
 function config-mixtile-blade3 {
 	
-	DEVICETREE="$DTB_BLADE3"
+	DEVICETREE="dtbs/rockchip/rk3588-blade3-v101-linux.dtb"
 	IMAGEFILE="${IMGPREFIX}-Mixtile_Blade3-${PROFILE}-UEFI.img"
 }
 
--- configs/installerconfigs.sh
+++ configs/installerconfigs.sh
@@ -3,7 +3,7 @@
 set -eE 
 trap 'echo Error: in $0 on line $LINENO' ERR
 
-#Installer variables get se here.
+#Installer variables get set here.
 #Unless you know what you are doing, DO NOT modify these.
 
 #Working directory stuff
@@ -14,6 +14,7 @@ NEWBOOTFSDIR="root/boot"
 DLTMP="download-tmp"
 OVERLAYDIR="overlays"
 PROFILEDIR="profiles"
+OUTDIR="outputs"
 
 #Rootfs
 ROOTFS_URL="http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz"
@@ -22,19 +23,6 @@ ROOTFS_TARBALL="ArchLinuxARM-aarch64-latest.tar.gz"
 #Grub efi directory: DO NOT CHANGE THIS.
 EFIDIR="boot"
 
-#platform devicetrees
-DTB_OPI5PLUS="dtbs/rockchip/rk3588-orangepi-5-plus.dtb"
-DTB_ROCK5B="dtbs/rockchip/rk3588-rock-5b.dtb"
-DTB_OPI5="dtbs/rockchip/rk3588s-orangepi-5.dtb"
-DTB_OPI5B="dtbs/rockchip/rk3588s-orangepi-5b.dtb"
-DTB_KHADAS_E2="dtbs/rockchip/rk3588s-khadas-edge2.dtb"
-DTB_ROCK5A="dtbs/rockchip/rk3588s-rock-5a.dtb"
-DTB_NANOPI_R6C="dtbs/rockchip/rk3588s-nanopi-r6c.dtb"
-DTB_NANOPI_R6S="dtbs/rockchip/rk3588s-nanopi-r6s.dtb"
-DTB_NANOPC_T6="dtbs/rockchip/rk3588-nanopc-t6.dtb"
-DTB_BLADE3="dtbs/rockchip/rk3588-blade3-v101-linux.dtb"
-DTB_IDNOVA="dtbs/rockchip/rk3588s-9tripod-linux.dtb"
-
 #image filename prefixes
 IMGPREFIX="ArchLinuxARM"
-OUTDIR="outputs"
+
