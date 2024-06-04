fd64a8cd9a93ee0114d3d8583179bdb60815610c
SputnikRocket <sputnikchicken08@gmail.com>
2024-06-04 18:15:29 -0400

    reduce to build only generic target

--- runscripts/inputhandler.sh
+++ runscripts/inputhandler.sh
@@ -25,19 +25,7 @@ then
 	clear
 	echo "Choose which board to install for by entering it's number below"
 	echo "---------------------"
-	echo " 1. Orange Pi 5 Plus"
-	echo " 2. Orange Pi 5"
-	echo " 3. Orange Pi 5 B"
-	echo " 4. Radxa Rock 5 A"
-	echo " 5. Radxa Rock 5 B"
-	echo " 6. Indiedroid Nova"
-	echo " 7. NanoPi R6C"
-	echo " 8. NanoPi R6S"
-	echo " 9. NanoPC T6"
-	echo " 10. Khadas Edge 2"
-	echo " 11. Mixtile Blade 3"
-	echo """ 12. None (only if you want to use devicetrees
- built into uefi or want to configure your own)"""
+	echo " 1. None"
 	echo "---------------------"
 	read -p "Board: " BOARDINDEX
 fi
@@ -70,51 +58,7 @@ else
 fi
 	
 #set BOARD config based off of script arguments or interactive input	
-if [[ ${BOARDINDEX} == "1" ]] || [[ ${BOARD} = "orangepi-5plus" ]]
-then
-	config-orangepi-5plus
-
-elif  [[ ${BOARDINDEX} == "2" ]] || [[ ${BOARD} = "orangepi-5" ]]
-then
-	config-orangepi-5
-
-elif  [[ ${BOARDINDEX} == "3" ]] || [[ ${BOARD} = "orangepi-5b" ]]
-then
-	config-orangepi-5b
-	
-elif  [[ ${BOARDINDEX} == "4" ]] || [[ ${BOARD} = "rock-5a" ]]
-then
-	config-rock-5a
-	
-elif  [[ ${BOARDINDEX} == "5" ]] || [[ ${BOARD} = "rock-5b" ]]
-then
-	config-rock-5b
-	
-elif  [[ ${BOARDINDEX} == "6" ]] || [[ ${BOARD} = "indiedroid-nova" ]]
-then
-	config-indiedroid-nova
-	
-elif  [[ ${BOARDINDEX} == "7" ]] || [[ ${BOARD} = "nanopi-r6c" ]]
-then
-	config-nanopi-r6c
-	
-elif  [[ ${BOARDINDEX} == "8" ]] || [[ ${BOARD} = "nanopi-r6s" ]]
-then
-	config-nanopi-r6s
-	
-elif  [[ ${BOARDINDEX} == "9" ]] || [[ ${BOARD} = "nanopc-t6" ]]
-then
-	config-nanopc-t6
-	
-elif  [[ ${BOARDINDEX} == "10" ]] || [[ ${BOARD} = "khadas-edge2" ]]
-then
-	config-khadas-edge2
-	
-elif  [[ ${BOARDINDEX} == "11" ]] || [[ ${BOARD} = "mixtile-blade3" ]]
-then
-	config-mixtile-blade3
-	
-elif  [[ ${BOARDINDEX} == "12" ]] || [[ ${BOARD} = "none" ]]
+if [[ ${BOARDINDEX} == "1" ]] || [[ ${BOARD} = "none" ]]
 then
 	config-none
 	
