4e55acdf24602d77af35da72c74e8c3da1fdae88
SputnikRocket <sputnikchicken08@gmail.com>
2024-06-04 18:18:48 -0400

    clean up some unneeeded stuff

--- configs/userconfigs.sh
+++ configs/userconfigs.sh
@@ -4,7 +4,5 @@
 INSTALL_LOCALE="en_US.UTF-8"
 INSTALL_LOCALE_ENC="UTF-8"
 
-#BredOS repo setup
-SETUP_BREDOS="yes"
 
 
--- runscripts/fixes.sh
+++ runscripts/fixes.sh
@@ -4,7 +4,3 @@ set -eE
 trap 'echo Error: in $0 on line $LINENO' ERR
 
 #apply fixes
-
-#fix iwlwifi issue "ish"
-mv ${WORKDIR}/${ROOTFSDIR}/lib/firmware/iwlwifi-ty-a0-gf-a0.pnvm ${WORKDIR}/${ROOTFSDIR}/lib/firmware/iwlwifi-ty-a0-gf-a0.pnvm.bak
-sync
