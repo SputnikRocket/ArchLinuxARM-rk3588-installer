f118dc07d2754821574367ecc9d00589327f6104
SputnikRocket <sputnikchicken08@gmail.com>
2024-06-04 18:24:21 -0400

    install all needed packages and clean up some junk

--- profiles/minimal.profile/install.sh
+++ profiles/minimal.profile/install.sh
@@ -3,8 +3,24 @@
 set -eE 
 trap 'echo Error: in $0 on line $LINENO' ERR
 
+#check if overlays exist before apply
+check-if-exists "${OVERLAYDIR}/overlay.${PROFILE}"
+
+#apply overlay
+apply-overlay "${WORKDIR}" "${PROFILE}"
+sync
+
+# add kwankiu's pacman repo
+pac-add-key "${WORKDIR}" "B669E3B56B3DC918"
+pac-add-repo "${WORKDIR}" "[experimental]" "Server = https://github.com/kwankiu/PKGBUILDs/releases/download/experimental"
+sync
+
 #do a pacman update for apparently no reason
 pac-update "${WORKDIR}"
+sync
+
+#install necessary packages
+pac-install-list "${WORKDIR}" "${PROFILEDIR}/${PROFILE}.profile/${PROFILE}.pkgs"
 sync
 
 #enable services for enabling usb 2 and resizing filesystem
--- profiles/xfce.profile/install.sh
+++ profiles/xfce.profile/install.sh
@@ -3,21 +3,6 @@
 set -eE 
 trap 'echo Error: in $0 on line $LINENO' ERR
 
-#update pacman lists
-pac-update "${WORKDIR}"
-sync
-
-#get packages for bred OS mirror and hardware desktop accel
-get-file "${WORKDIR}" "https://github.com/SputnikRocket/archlinuxarm-rk3588-pkgs/releases/download/latest/bredos-keyring-20230818-1-any.pkg.tar.xz"
-get-file "${WORKDIR}" "https://github.com/SputnikRocket/archlinuxarm-rk3588-pkgs/releases/download/latest/bredos-mirrorlist-20230818-1-any.pkg.tar.xz"
-get-file "${WORKDIR}" "https://github.com/SputnikRocket/archlinuxarm-rk3588-pkgs/releases/download/latest/mali-g610-firmware-1.0.2-1-any.pkg.tar.zst"
-sync
-
-#install bredOS stuff
-pac-install-local "${WORKDIR}" "bredos-keyring-20230818-1-any.pkg.tar.xz"
-pac-install-local "${WORKDIR}" "bredos-mirrorlist-20230818-1-any.pkg.tar.xz"
-sync
-
 #check if overlays exist before apply
 check-if-exists "${OVERLAYDIR}/overlay.${PROFILE}"
 
@@ -25,7 +10,17 @@ check-if-exists "${OVERLAYDIR}/overlay.${PROFILE}"
 apply-overlay "${WORKDIR}" "${PROFILE}"
 sync
 
-#update again
+# add kwankiu's pacman repo
+pac-add-key "${WORKDIR}" "B669E3B56B3DC918"
+pac-add-repo "${WORKDIR}" "[experimental]" "Server = https://github.com/kwankiu/PKGBUILDs/releases/download/experimental"
+sync
+
+# add 7Ji's pacman repo
+pac-add-key "${WORKDIR}" "BA27F219383BB875"
+pac-add-repo "${WORKDIR}" "[7Ji]" "Server = https://github.com/7Ji/archrepo/releases/download/$arch"
+sync
+
+#update
 pac-update "${WORKDIR}"
 sync
 
@@ -33,17 +28,11 @@ sync
 pac-install-list "${WORKDIR}" "${PROFILEDIR}/${PROFILE}.profile/${PROFILE}.pkgs"
 sync
 
-#install panfork
-pac-install-local "${WORKDIR}" "mali-g610-firmware-1.0.2-1-any.pkg.tar.zst"
-sync
-
 #enable and disable services
 systemd-enable "${WORKDIR}" "resize-filesystem.service"
 systemd-enable "${WORKDIR}" "enable-usb2.service"
 systemd-enable "${WORKDIR}" "lightdm.service"
-systemd-enable "${WORKDIR}" "set-mali-firmware.service"
 systemd-enable "${WORKDIR}" "NetworkManager.service"
 systemd-disable "${WORKDIR}" "NetworkManager-wait-online.service"
-systemd-enable "${WORKDIR}" "bluetooth.service"
-#systemd-enable "${WORKDIR}" "xdg-user-dirs-update.service" 
+systemd-enable "${WORKDIR}" "bluetooth.service" 
 sync
