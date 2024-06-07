ae92b69cc0ea7295b688bb6a1c8f49acc46ac580
SputnikRocket <sputnikchicken08@gmail.com>
2024-06-07 18:25:19 -0400

    fix grub cannot find nonexistent module

--- scripts/grubsetup.sh
+++ scripts/grubsetup.sh
@@ -14,20 +14,7 @@ function install-grub() {
 	--directory=/usr/lib/grub/arm64-efi/                            \
 	--removable                                                     \
 	--sbat /usr/share/grub/sbat.csv                                 \
-	--modules="all_video boot btrfs cat chain configfile echo       \
-	efifwsetup efinet ext2 fat font gettext gfxmenu gfxterm         \
-	gfxterm_background gzio halt help hfsplus iso9660 jpeg          \ 
-	keystatus loadenv loopback linux ls lsefi lsefimmap lsefisystab \
-	lssal memdisk minicmd normal ntfs part_apple part_msdos         \
-	part_gpt password_pbkdf2 png probe reboot regexp search         \
-	search_fs_uuid search_fs_file search_label sleep smbios squash4 \
-	test true video xfs zfs zfscrypt zfsinfo                        \
-	cryptodisk gcry_arcfour gcry_blowfish gcry_camellia gcry_cast5  \
-	gcry_crc gcry_des gcry_dsa gcry_idea gcry_md4 gcry_md5          \
-	gcry_rfc2268 gcry_rijndael gcry_rmd160 gcry_rsa gcry_seed       \ 
-	gcry_serpent gcry_sha1 gcry_sha256 gcry_sha512 gcry_tiger       \
-	gcry_twofish gcry_whirlpool luks lvm mdraid09 mdraid1x raid5rec \
-	raid6rec http tftp"
+	--modules="all_video boot btrfs cat chain configfile echo efifwsetup efinet ext2 fat font gettext gfxmenu gfxterm gfxterm_background gzio halt help hfsplus iso9660 jpeg keystatus loadenv loopback linux ls lsefi lsefimmap lsefisystab lssal memdisk minicmd normal ntfs part_apple part_msdos part_gpt password_pbkdf2 png probe reboot regexp search search_fs_uuid search_fs_file search_label sleep smbios squash4 test true video xfs zfs zfscrypt zfsinfo cryptodisk gcry_arcfour gcry_blowfish gcry_camellia gcry_cast5 gcry_crc gcry_des gcry_dsa gcry_idea gcry_md4 gcry_md5 gcry_rfc2268 gcry_rijndael gcry_rmd160 gcry_rsa gcry_seed gcry_serpent gcry_sha1 gcry_sha256 gcry_sha512 gcry_tiger gcry_twofish gcry_whirlpool luks lvm mdraid09 mdraid1x raid5rec raid6rec http tftp"
 }
 
 #generate main grub config
