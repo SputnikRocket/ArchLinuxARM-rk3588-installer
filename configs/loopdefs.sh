553d4db4810a1f814064eff84117b2158ea5b8e7
SputnikRocket <sputnikchicken08@gmail.com>
2024-07-03 13:43:46 -0400

    change image naming system a little bit

--- configs/loopdefs.sh
+++ configs/loopdefs.sh
@@ -5,4 +5,4 @@ trap 'echo Error: in $0 on line $LINENO' ERR
 
 #image filename settings
 IMGPREFIX="ArchLinuxARM"
-IMAGEFILE="${IMGPREFIX}-${IMGPLATFORMNAME}-${PROFILE}-UEFI.img"
+IMAGEFILE="${IMGPREFIX}-${PLATFORM}-${PROFILE}-UEFI.img"
