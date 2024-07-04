e8c60798660b0610905aaacb157116e19f096c53
SputnikRocket <sputnikchicken08@gmail.com>
2024-07-04 11:04:05 -0400

    rearrange configuration order to fix path not found error

--- runscripts/importconfscripts.sh
+++ runscripts/importconfscripts.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
 
+set -eE
+trap 'echo Error: in $0 on line $LINENO' ERR
+
+#check if distro conf exists
+check-if-exists "configs/distro/${DISTRO}.sh"
+source-script "configs/distro/${DISTRO}.sh"
 
 #check if profile exists and import setting
 check-if-exists "${PROFILEDIR}/${PROFILE}"
@@ -7,7 +14,3 @@ source-script "${PROFILEDIR}/${PROFILE}/install.sh"
 #check if platform exists and import settings
 check-if-exists "${PLATFORMDIR}/${PLATFORM}"
 source-script "${PLATFORMDIR}/${PLATFORM}/install.sh"
-
-#check if distro conf exists
-check-if-exists "configs/distro/${DISTRO}.sh"
-source-script "configs/distro/${DISTRO}.sh"
