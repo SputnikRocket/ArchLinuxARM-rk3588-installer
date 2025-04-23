6d40b328c42dc82f732390899d399a1359e2d41b
SputnikRocket <sputnikchicken08@gmail.com>
2025-04-23 14:45:49 -0400

    Wipe to start clean

--- configs/general/loopdefs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -eE 
-trap 'echo Error: in $0 on line $LINENO' ERR
-
-#image filename settings
-IMAGEFILE="${DISTRO}-${PLATFORM}-${PROFILE}-uefi.img"
--- runscripts/bootloadersetup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eE 
-trap 'echo Error: in $0 on line $LINENO' ERR
-
-#run platform bootloader hook
-
-#bootloader setup
-bootloader-platform-hook
