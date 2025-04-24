746d98f38f8050c6bb80f55b24b6bf4af82fdbb0
SputnikRocket <sputnikchicken08@gmail.com>
2025-04-24 18:38:43 -0400

    Add image output path

--- config/installer/paths.sh
+++ config/installer/paths.sh
@@ -1,15 +1,15 @@
-#!/bin/env bash
+#!/bin/bash
 
-set -eE 
-trap 'echo Error: in $0 on line $LINENO' ERR
+set -eE
+trap 'Print-Error " in api/disk.sh on line ${LINENO}"' ERR
 
-#Installer root path
+# Installer root path
 INSTALLER_ROOT="$(pwd)"
 
-#Installer api path
+# Installer api path
 API_PATH="${INSTALLER_ROOT}/api"
 
-#Installer configuration paths
+# Installer configuration paths
 CONFIG_PATH="${INSTALLER_ROOT}/config"
 CONFIG_INSTALLER_PATH="${CONFIG_PATH}/installer"
 CONFIG_PLATFORM_PATH="${CONFIG_PATH}/platforms"
@@ -18,10 +18,13 @@ CONFIG_USER_PATH="${CONFIG_PATH}/users"
 CONFIG_BOOTLOADER_PATH="${CONFIG_PATH}/bootloaders"
 CONFIG_DISKLAYOUT_PATH="${CONFIG_PATH}/disklayouts"
 
-#Installer working directory paths
+# Installer working directory paths
 WORKDIR_PATH="${INSTALLER_ROOT}/work"
 WORKDIR_DISKFS_PATH="${WORKDIR_PATH}/diskfs"
 WORKDIR_ROOTFS_PATH="${WORKDIR_PATH}/rootfs"
 WORKDIR_TMP_PATH="${WORKDIR_PATH}/tmp"
 WORKDIR_DOWNLOAD_PATH="${WORKDIR_TMP_PATH}/download"
 WORKDIR_TRANSIENT_PATH="${WORKDIR_TMP_PATH}/transient"
+
+# Image output path
+OUTPUT_PATH="${INSTALLER_ROOT}/outputs"
