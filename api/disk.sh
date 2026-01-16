9a9440f1ed2018467abb355716537e0b487a5ec0
SputnikRocket <sputnikchicken08@gmail.com>
2026-01-16 17:14:03 -0500

    Initial BTRFS and subvolume support

--- api/disk.sh
+++ api/disk.sh
@@ -79,7 +79,7 @@ function Setup-Disk () {
 		Yaml-Element-GetVal "${ConfigYaml}" ".partitions.${Part}.mountopts.check"
 		local PartMountCheck="${YamlOutput}"
 		
-		
+				
 		# Generate filesystem UUID
 		Print-Debug "Generating UUID for filesystem..." 1
 		if [[ "${PartFsType}" == "vfat" ]]
@@ -109,6 +109,7 @@ function Setup-Disk () {
 		
 		fi
 		
+		
 		# Add label to partition if specified
 		if [[ "${PartLabel}" != "null" ]]
 		then
@@ -117,6 +118,7 @@ function Setup-Disk () {
 			sync
 			
 		fi
+		
 		
 		# Format partitions
 		if [[ "${PartFsType}" == "vfat" ]]
@@ -143,12 +145,77 @@ function Setup-Disk () {
 			yes | mkfs.btrfs -f -U "${PartUuid}" "${DiskDevice}${PartSeparator}${PartNum}"
 			sync
 			
+			
+			# Check if the BTRFS filesystem has subvolumes
+			Yaml-Element-GetVal "${ConfigYaml}" ".partitions.${Part}.hassubvols"
+			local HasSubVols="${YamlOutput}"
+			
+			
+			# Create subvolumes if above is yes 
+			if [[ "${HasSubVols}" == "yes" ]]
+			then
+				# Get list of subvolumes
+				Yaml-Element-GetSubLists "${ConfigYaml}" ".partitions.${Part}.subvols"
+				local SubVols="${YamlOutput}"
+				
+				
+				# Mount BTRFS parent volume
+				mount "${DiskDevice}${PartSeparator}${PartNum}" "${WORKDIR_DISKFS_PATH}"
+				
+				
+				# Recurse through subvolume list, creating each one
+				for SubVol in ${SubVols}
+				do
+					# Get subvolume details
+					Yaml-Element-GetVal "${ConfigYaml}" ".partitions.${Part}.subvols.${SubVol}.name"
+					local SubVolName="${YamlOutput}"
+					
+					Yaml-Element-GetVal "${ConfigYaml}" ".partitions.${Part}.subvols.${SubVol}.mountopts.path"
+					local SubVolMountPath="${YamlOutput}"
+		
+					Yaml-Element-GetVal "${ConfigYaml}" ".partitions.${Part}.subvols.${SubVol}.mountopts.flags"
+					local SubVolMountFlags="${YamlOutput}"
+		
+					Yaml-Element-GetVal "${ConfigYaml}" ".partitions.${Part}.subvols.${SubVol}.mountopts.backup"
+					local SubVolMountBackup="${YamlOutput}"
+		
+					Yaml-Element-GetVal "${ConfigYaml}" ".partitions.${Part}.subvols.${SubVol}.mountopts.check"
+					local SubVolMountCheck="${YamlOutput}"
+					
+					
+					# Create subvolume
+					btrfs subvolume create "${WORKDIR_DISKFS_PATH}/${SubVolName}"
+					
+					
+					# Check if subvolume doesn't have a mountpoint
+					if [[ "${SubVolMountPath}" != "null" ]]
+					then
+						# Create fstab entry and mount path
+						Print-Debug "Adding entry for ${SubVolMountPath} to base fstab..." 3
+						echo "UUID=${FstabUuid}	${SubVolMountPath}	${PartFsType}	${SubVolMountFlags},subvol=/${SubVolName}	${SubVolMountBackup}	${SubVolMountCheck}" >> "${WORKDIR_TRANSIENT_PATH}/fstab_initial"
+						echo "${SubVolMountPath}" >> "${WORKDIR_TRANSIENT_PATH}/mounts_initial"
+			
+					fi
+					
+				done
+				
+				
+				# Unmount BTRFS parent volume
+				umount "${DiskDevice}${PartSeparator}${PartNum}"
+				
+			fi
+				
 		fi
 		
-		# Create fstab entry and mount path
-		Print-Debug "Adding entry for ${PartMountPath} to base fstab..." 3
-		echo "UUID=${FstabUuid}	${PartMountPath}	${PartFsType}	${PartMountFlags}	${PartMountBackup}	${PartMountCheck}" >> "${WORKDIR_TRANSIENT_PATH}/fstab_initial"
-		echo "${PartMountPath}" >> "${WORKDIR_TRANSIENT_PATH}/mounts_initial"
+		# Check if partition doesn't have a mount point or is BTRFS with subvolumes
+		if [[ "${PartMountPath}" != "null" ]] || [[ "${HasSubVols}" != "yes" ]]
+		then
+			# Create fstab entry and mount path
+			Print-Debug "Adding entry for ${PartMountPath} to base fstab..." 3
+			echo "UUID=${FstabUuid}	${PartMountPath}	${PartFsType}	${PartMountFlags}	${PartMountBackup}	${PartMountCheck}" >> "${WORKDIR_TRANSIENT_PATH}/fstab_initial"
+			echo "${PartMountPath}" >> "${WORKDIR_TRANSIENT_PATH}/mounts_initial"
+			
+		fi
 		
 	done
 	
