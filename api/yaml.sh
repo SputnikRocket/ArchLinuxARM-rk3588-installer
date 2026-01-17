4a98a399b2df80c0057c66d2fa1b1b02456de61e
SputnikRocket <sputnikchicken08@gmail.com>
2026-01-17 00:29:20 -0500

    Add list merger function to yaml parser module

--- api/yaml.sh
+++ api/yaml.sh
@@ -8,13 +8,15 @@ function Yaml-Element-GetSubLists () {
 	
 	local YamlFile=${1}
 	local Address=${2}
+	
 	local Elements
+	export YamlOutput
 	
 	Print-Debug "Getting Elements from ${YamlFile} at ${Address}" 3
 	Elements=$(yq -y "${Address}" < "${YamlFile}" | sed "/\ \ .*/d" | sed "s|:||g")
 	
 	Print-Debug "Retreived Elements:\n${Elements}" 4
-	export YamlOutput="${Elements}"
+	YamlOutput="${Elements}"
 }
 
 # Return value from element
@@ -22,13 +24,15 @@ function Yaml-Element-GetVal () {
 	
 	local YamlFile=${1}
 	local Address=${2}
+	
 	local Element
+	export YamlOutput
 
 	Print-Debug "Getting Element from ${YamlFile} at ${Address}" 3
 	Element=$(yq -y "${Address}" < "${YamlFile}" | sed "/\.\.\./d" | sed "s|'||g")
 	
 	Print-Debug "Retreived Element: ${Element}" 4
-	export YamlOutput="${Element}"
+	YamlOutput="${Element}"
 }
 
 # Return values from list
@@ -36,13 +40,40 @@ function Yaml-Element-GetList () {
 	
 	local YamlFile=${1}
 	local Address=${2}
+	
 	local Elements
+	export YamlOutput
 	
 	Print-Debug "Getting Elements from ${YamlFile} at ${Address}" 3
 	Elements=$(yq -y "${Address}" < "${YamlFile}" | sed "s|-\ ||g")
 	
 	Print-Debug "Retreived Elements:\n${Elements}" 4
-	export YamlOutput="${Elements}"
+	YamlOutput="${Elements}"
+}
+
+# Merge lists and eliminate duplicates
+function Yaml-Element-MergeLists () {
+	
+	local Address=${1}
+	local YamlFiles=($(echo "${@}" | sed "s|${1}\ ||g"))
+	
+	local Elements
+	local MergedElements
+	export YamlOutput
+	
+	for YamlFile in "${YamlFiles[@]}"
+	do
+		Print-Debug "Getting Elements from ${YamlFile} at ${Address}" 3
+		Elements=$(yq -y "${Address}" < "${YamlFile}" | sed "s|-\ ||g")
+		
+		Print-Debug "Retreived Elements:\n${Elements}" 4
+	    MergedElements="${MergedElements}\n${Elements}"
+	done
+	
+	MergedElements=$(echo "${MergedElements}" | sort -u | sed "/^$/d")
+	
+	Print-Debug "Merged Elements:\n${MergedElements}" 4
+	YamlOutput="${MergedElements}"
 }
 
 trap '' EXIT
