cf80e381a0f66b3662f02ebd81b3b1ede9b8c591
SputnikRocket <sputnikchicken08@gmail.com>
2026-01-18 17:00:18 -0500

    Add presetup module to test script and use workdirs creation function

--- bootstrap_test.sh
+++ bootstrap_test.sh
@@ -5,12 +5,11 @@ trap 'echo Error: in $0 on line $LINENO' ERR
 
 Debug=4
 
-source "$(pwd)/api/debugerr.sh"
-
 source "$(pwd)/config/installer/paths.sh"
 
-mkdir -p "${WORKDIR_TRANSIENT_PATH}"
-mkdir -p "${WORKDIR_DISKFS_PATH}"
-
+source "${API_PATH}/debugerr.sh"
 source "${API_PATH}/yaml.sh"
+source "${API_PATH}/presetup.sh"
 source "${API_PATH}/disk.sh"
+
+Create-Workdirs "${CONFIG_INSTALLER_PATH}/paths.sh"
